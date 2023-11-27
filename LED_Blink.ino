45f89898697e7c49f905c556ec585823a80ce055
Danielle <ntdoyds@gmail.com>
2023-11-27 11:11:34 -0500

    Added a third LED and programmed it to blink.

--- LED_Blink.ino
+++ LED_Blink.ino
@@ -1,15 +1,19 @@
 int ledPin = 7;
 int ledPin1 = 9;
+int ledPin2 = 11;
 
 void setup() {
   pinMode(ledPin);
   pinMode(ledPin1);
+  pinMode(ledPin2);
 }
 
 void loop() {
   digitalWrite(ledPin, HIGH);
   digitalWrite(ledPin1, HIGH);
+  digitalWrite(ledPin2, HIGH);
   delay(500);
   digitalWrite(ledPin, LOW);
   digitalWrite(ledPin1, LOW);
+  digitalWrite(ledPin2, LOW);
 }
