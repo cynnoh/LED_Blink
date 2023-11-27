71bc93b11100e1c763be9e33c4885ecb0693d7a7
Danielle <ntdoyds@gmail.com>
2023-11-27 11:01:50 -0500

    Added LED.

--- LED_Blink.ino
+++ LED_Blink.ino
@@ -1,9 +1,8 @@
-void setup() {
-  // put your setup code here, to run once:
+int ledPin = 7;
 
+void setup() {
+  pinMode(ledPin);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
 }
